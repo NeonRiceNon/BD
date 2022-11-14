f686e61622e1e039deb84a744e6d85bc1fcf681a
NeonRiceNon <liliyahaill89@gmail.com>
2022-11-14 11:09:42 +0300

    123

--- BD-main/BD/src/Connect.java
+++ BD-main/BD/src/Connect.java
@@ -1,16 +1,33 @@
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Connect {
-    public void Connect (String username, String pass, String jdbcURL){
+    public static Connection Connect (String username, String pass, String jdbcURL){
 
         try {
             java.sql.Connection connection = DriverManager.getConnection(jdbcURL, pass, username);
-            System.out.println(" Подключился к серверу postgresql ");
-            connection.close();
+            System.out.println(" kkkk postgresql ");
+            return connection;
         } catch (SQLException e){
             System.out.println(" Ошибка подключения к серверу postgresql ");
             e.printStackTrace();
+            return null;
         }
+    }
+
+    public static void INSERT() {
+
+
+
+        try {
+            Statement statement = Connect("PKS","PKS", "jdbc:postgresql://46.229.214.241:5432/book publishing").createStatement();
+            int rows = statement.executeUpdate("INSERT INTO public.\"Author\" (name) VALUES ('Пушкин')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- BD-main/BD/src/Main.java
+++ BD-main/BD/src/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+       /* Scanner scanner = new Scanner(System.in);
         System.out.println("Enter login");
         String username = scanner.nextLine();
         System.out.println("Enter password");
-        String pass = scanner.nextLine();
+        String pass = scanner.nextLine();*/
 
-        Connect con = new Connect();
-        con.Connect(username, pass, "jdbc:postgresql://46.229.214.241:5432/postgres");
+        Connect.INSERT();
     }
 }
