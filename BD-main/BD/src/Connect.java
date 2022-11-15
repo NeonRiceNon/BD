f7475db07bc7e85c6d68337f3a46ebfeecdd037f
NeonRiceNon <liliyahaill89@gmail.com>
2022-11-15 09:27:21 +0300

    456

--- BD-main/BD/src/Connect.java
+++ BD-main/BD/src/Connect.java
@@ -20,11 +20,33 @@ public class Connect {
 
     public static void INSERT() {
 
-
-
         try {
             Statement statement = Connect("PKS","PKS", "jdbc:postgresql://46.229.214.241:5432/book publishing").createStatement();
-            int rows = statement.executeUpdate("INSERT INTO public.\"Author\" (name) VALUES ('Пушкин')");
+            int rows1 = statement.executeUpdate("INSERT INTO public.\"Author\" (name) VALUES ('Пушкин')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            Statement statement = Connect("PKS","PKS", "jdbc:postgresql://46.229.214.241:5432/book publishing").createStatement();
+            int rows2 = statement.executeUpdate("INSERT INTO public.\"Client\" (\"Shop\",\"Address\",\"Phone_number\") VALUES ('Книга','ул.Синяя, д.10','+79995553535')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            Statement statement = Connect("PKS","PKS", "jdbc:postgresql://46.229.214.241:5432/book publishing").createStatement();
+            int rows3 = statement.executeUpdate("INSERT INTO public.\"Genre\" (name) VALUES ('Роман')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            Statement statement = Connect("PKS","PKS", "jdbc:postgresql://46.229.214.241:5432/book publishing").createStatement();
+            int rows4 = statement.executeUpdate("INSERT INTO public.\"Books\" (\"Title_of_the_book\") VALUES ('2022-10-23','100')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            Statement statement = Connect("PKS","PKS", "jdbc:postgresql://46.229.214.241:5432/book publishing").createStatement();
+            int rows5 = statement.executeUpdate("INSERT INTO public.\"Books\" (\"Order_date\",\"Number_of_copies\") VALUES ('2022-10-23','100')");
         } catch (SQLException e) {
             e.printStackTrace();
         }
